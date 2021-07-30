8493543189fe04f5b72e8da3e394d27bcaf5dc3a
Fallenefc <arylmoraesn@gmail.com>
2021-07-30 16:24:37 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Header from "./components/header/Header.svelte";
+
+	export let name: string;
+</script>
+
+<main>
+	<Header />
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="scss">
+	:global(body, html) {
+		padding: 0;
+		margin: 0;
+		font-family: 'Quicksand', sans-serif;
+		background-color: #25282c;
+		color: #fff;
+	}
+	:global(html) {
+		font-size: 62.5%;
+	}
+	:global(*) {
+  		box-sizing: border-box;
+	}
+</style>
---
+++ src/components/header/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<main class="header__container">
+   <div>Logo</div>
+   <div>Menu</div>
+</main>
+
+<style lang="scss">
+   .header {
+      &__container {
+         background-color: rgb(29, 28, 28);
+         padding: 2rem;
+         height: 7vh;
+         display: flex;
+         justify-content: space-between;
+         align-items: center;
+         font-size: 1.6rem;
+         color: rgb(224, 224, 33);
+         font-weight: 600;
+      }
+   }
+</style>
