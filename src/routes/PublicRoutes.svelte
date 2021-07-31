aa8eb2f1cd80082da91b974996a6bd4d5bab0bb9
Fallenefc <arylmoraesn@gmail.com>
2021-07-31 13:25:50 -0600

    added 404 page

--- src/routes/PublicRoutes.svelte
+++ src/routes/PublicRoutes.svelte
@@ -2,13 +2,15 @@
    import { Router, Route } from "svelte-routing";
    import Home from "../pages/Home.svelte";
    import Login from "../pages/Login.svelte";
+   import NotFound from "../pages/NotFound.svelte";
    import Signup from "../pages/Signup.svelte";
    </script>
    
    <Router>
-      <Route path="/"><Home name="Boi Gaming"/></Route>
+      <Route path="/" exact><Home name="Boi Gaming"/></Route>
       <Route path="/login"><Login/></Route>
       <Route path="/signup"><Signup/></Route>
+      <Route component="{NotFound}" />
    </Router>
    
    <style lang="scss">
